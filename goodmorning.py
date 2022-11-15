7721d1f596e0b2336ed708a131146e37c91ebe53
fenirani <fenirani@live.com>
2022-11-15 13:19:23 -0500

    Add Main Python Code

---
+++ goodmorning.py
@@ -0,0 +1,14 @@
+import webbrowser
+
+# Open all the websites that you normally open
+
+# Email
+webbrowser.open('https://outlook.live.com/mail/0/')
+# Weather
+webbrowser.open('https://weather.com/weather/today/l/f0c3f5c8ac51cad42aa523dbec6b1935ec08635477be5cfb75db030e985e14af')
+# Facebook
+webbrowser.open('https://www.facebook.com/')
+
+# Open New Window
+# Guestcenter (Work)
+webbrowser.open_new('https://guestcenter.opentable.com/restaurant/218437/front-of-house#/reservations/live')
