0c4f8a41579d957d50c6a310dbdf108bbdddd62a
zalir <30611163+mektievp@users.noreply.github.com>
2022-12-06 19:33:39 -0500

    Update goodmorning.py
    better way to start the morning if you ask me

--- goodmorning.py
+++ goodmorning.py
@@ -3,14 +3,14 @@ import webbrowser
 # Open all the websites that you normally open
 
 # Email
-webbrowser.open('https://outlook.live.com/mail/0/')
+webbrowser.open('https://www.joerogan.com/')
 # Weather
-webbrowser.open('https://weather.com/weather/today/l/f0c3f5c8ac51cad42aa523dbec6b1935ec08635477be5cfb75db030e985e14af')
+webbrowser.open('https://www.jordanbpeterson.com/')
 # Facebook
-webbrowser.open('https://www.facebook.com/')
+webbrowser.open('https://www.liverking.com/')
 # Reddit
-webbrowser.open('https://old.reddit.com')
+webbrowser.open('https://lexfridman.com/')
 
 # Open New Window
 # Guestcenter (Work)
-webbrowser.open_new('https://guestcenter.opentable.com/restaurant/218437/front-of-house#/reservations/live')
+webbrowser.open_new('https://www.moscowtimes.ru/')
